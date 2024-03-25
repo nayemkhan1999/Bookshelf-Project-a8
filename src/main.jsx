77b4e487a9e9c9830854a309ca76211687653f9d
nayemkhan1999 <md.naiemkhan1999@gmail.com>
2024-03-25 19:38:36 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
