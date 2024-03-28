0c1602ea53c4358b42900497cd1445452296432b
nayemkhan1999 <md.naiemkhan1999@gmail.com>
2024-03-28 17:15:25 +0600

    added a readme file edit

--- src/Components/Hook/useHookData.jsx
+++ src/Components/Hook/useHookData.jsx
@@ -6,7 +6,7 @@ const useHookData = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetch("./fake-data.json")
+    fetch("/fake-data.json")
       .then((res) => res.json())
       .then((data) => setCard(data));
     setLoading(false);
