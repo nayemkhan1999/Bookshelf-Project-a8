8c2d9aab7c6d582f311e6c869f875e51c9bad66b
nayemkhan1999 <md.naiemkhan1999@gmail.com>
2024-03-28 15:35:59 +0600

    project complite and published

--- src/Components/BuyBook/BuyBook.jsx
+++ src/Components/BuyBook/BuyBook.jsx
@@ -1,7 +1,26 @@
+import tow200 from "../../../public/tow200.jpg";
 const BuyBook = () => {
   return (
     <div>
-      <h2>Helo</h2>
+      <div className="lg:mx-16 md:mx-2 font-work mt-3">
+        <div
+          style={{
+            backgroundImage: `url(${tow200})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="hero h-[85vh]  bg-[#F2F2F2] rounded-xl"
+        >
+          <div className="hero-content flex-col lg:flex-row-reverse gap-20">
+            <div className="">
+              <button className="btn btn-active text-white font-semibold bg-gray-800 border-none mt-96 text-lg">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
